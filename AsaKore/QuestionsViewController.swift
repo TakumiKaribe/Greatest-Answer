04e699bb0fafca95587671b54c7bea6c315bb8d2
takumi karibe <takumi.k.5610@gmail.com>
2018-05-23 00:19:44 +0900

    移植

---
+++ AsaKore/QuestionsViewController.swift
@@ -0,0 +1,81 @@
+//
+//  QuestionsViewController.swift
+//  ThisIsTheCorrectAnswerUntilMorning
+//
+//  Created by 刈部拓未 on 2017/11/04.
+//  Copyright © 2017年 刈部拓未. All rights reserved.
+//
+
+import UIKit
+
+/// QuestionsViewController
+class QuestionsViewController: UIViewController {
+    @IBOutlet private weak var initialLabel: UILabel!
+    @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    
+    private var viewModel: QuestionsViewModel?
+    
+    private var time = 60
+    private var timer: Timer? = nil
+    
+    private var onTimerFlag = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = QuestionsViewModel()
+        applyLabel(index: 0)
+        apply(time: time)
+    }
+    
+    @IBAction func didTapStartButton(_ sender: Any) {
+        if !onTimerFlag {
+            onTimerFlag = true
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timeCount), userInfo: nil, repeats: true)
+        } else {
+            onTimerFlag = false
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    @IBAction func didTapResetButton(_ sender: Any) {
+        if onTimerFlag {
+            onTimerFlag = false
+            timer?.invalidate()
+            timer = nil
+        }
+        
+        time = 60
+        apply(time: time)
+    }
+    
+    @IBAction func didTapGenerateNextQuestionButton(_ sender: Any) {
+        guard let num = viewModel?.randNum() else { return }
+        applyLabel(index: num)
+    }
+}
+
+/// MARK: - Private Extension
+private extension QuestionsViewController {
+    func applyLabel(index: Int) {
+        guard let questions = viewModel?.questions else { return }
+        
+        let question = questions[index]
+        initialLabel.text = "「\(question.initial)」で始まる"
+        questionLabel.text = "\(question.question)は？"
+    }
+    
+    func apply(time: Int) {
+        timeLabel.text = "残り\(time)秒"
+    }
+    
+    @objc func timeCount() {
+        if time <= 0 {
+            timer?.invalidate()
+        }
+        
+        time -= 1
+        apply(time: time)
+    }
+}
---
+++ AsaKore/QuestionsViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  QuestionsViewModel.swift
+//  ThisIsTheCorrectAnswerUntilMorning
+//
+//  Created by 刈部拓未 on 2018/04/29.
+//  Copyright © 2018年 刈部拓未. All rights reserved.
+//
+
+import Foundation
+
+/// QuestionsViewModel
+struct QuestionsViewModel {
+    var questions = [Question]()
+    
+    init() {
+        guard let data = fetchJsonFile() else { return }
+        guard let questions = decodeFile(by: data) else { return }
+        self.questions = questions
+    }
+}
+
+// MARK: - Extension
+extension QuestionsViewModel {
+    func randNum() -> Int {
+        return Int(arc4random_uniform(UInt32(questions.count)))
+    }
+}
+
+
+// MARK: - Private Extension
+private extension QuestionsViewModel {
+    func fetchJsonFile() -> Data? {
+        do {
+            return try Data(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "PreparedProblems", ofType: "json")!))
+            
+        } catch {
+            return nil
+        }
+    }
+    
+    func decodeFile(by data: Data) -> [Question]? {
+        do {
+            return try JSONDecoder().decode([Question].self, from: data)
+            
+        } catch {
+            return nil
+        }
+    }
+}
