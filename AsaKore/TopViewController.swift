995f12c1cc779d39a19c8f37730785ca2828b448
Takumi Karibe <>
2018-06-02 16:39:03 +0900

    TopVCのTableViewのdatasourceとdelegate設定

--- AsaKore/TopViewController.swift
+++ AsaKore/TopViewController.swift
@@ -12,3 +12,17 @@ import UIKit
 final class TopViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
 }
+
+extension TopViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "QuestionCell")!
+    }
+}
