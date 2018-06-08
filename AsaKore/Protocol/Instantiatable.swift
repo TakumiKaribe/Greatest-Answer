236a061ea0e0b47263f3a2e6e0a9aebacb9ef763
Takumi Karibe <>
2018-06-08 15:06:13 +0900

    UI生成用のプロトコルを修正

---
+++ AsaKore/Protocol/Instantiatable.swift
@@ -0,0 +1,34 @@
+//
+//  Instantiatable.swift
+//  AsaKore
+//
+//  Created by 刈部拓未 on 2018/06/02.
+//  Copyright © 2018年 刈部拓未. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Instantiatable {}
+extension UIViewController: Instantiatable {}
+extension Instantiatable where Self: UIViewController {
+    static func instantiate() -> Self {
+        let className = String(describing: self)
+        let storyBoard = UIStoryboard(name: className, bundle: nil)
+        return storyBoard.instantiateViewController(withIdentifier: className) as! Self
+    }
+}
+
+extension UITableViewCell: Instantiatable {}
+extension Instantiatable where Self: UITableViewCell {
+    static func registered(in tableView: UITableView) {
+        let className = String(describing: self)
+        let nib = UINib(nibName: className, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: className)
+    }
+    
+    static func dequeue(from tableView: UITableView, at indexPath: IndexPath) -> Self {
+        let className = String(describing: self)
+        return tableView.dequeueReusableCell(withIdentifier: className, for: indexPath)
+    }
+}
