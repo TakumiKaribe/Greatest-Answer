905058876ff9c183fe5213a3ac6320332fc09361
Takumi Karibe <>
2018-06-08 16:11:14 +0900

    AdMobの設定ファイル用のラッパー作成

--- AsaKore/AppDelegate.swift
+++ AsaKore/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GADMobileAds.configure(withApplicationID: AdMobSettings.appId)
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UINavigationController(rootViewController: TopViewController.instantiate())
---
+++ AsaKore/Utility/AdMobSettings.swift
@@ -0,0 +1,26 @@
+//
+//  AdMobSettings.swift
+//  AsaKore
+//
+//  Created by 刈部拓未 on 2018/06/08.
+//  Copyright © 2018年 刈部拓未. All rights reserved.
+//
+
+import Foundation
+
+struct AdMobSettings {
+    static let filePath = Bundle.main.path(forResource: String(describing: AdMobSettings.self), ofType: "plist")
+    static let plist = NSDictionary(contentsOfFile: filePath!) ?? [:]
+    
+    static var appId: String {
+        return plist.object(forKey: "AppId") as? String ?? ""
+    }
+    
+    static var bannerId: String {
+        return plist.object(forKey: "BannerId") as? String ?? ""
+    }
+    
+    static var interstitialId: String {
+        return plist.object(forKey: "InterstitialId") as? String ?? ""
+    }
+}
